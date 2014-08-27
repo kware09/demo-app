dea589cb1360c8503bc7426d37b9f592c09d1837
Kieran Ware <kware@wishbone.ie>
2014-08-27 22:41:23 +0100

    Intial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name demoSassApp
+ * @description
+ * # demoSassApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('demoSassApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name demoSassApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the demoSassApp
+ */
+angular.module('demoSassApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
